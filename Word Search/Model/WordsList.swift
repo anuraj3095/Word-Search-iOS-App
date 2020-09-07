983270620ed26ecc7182facf15fe70655c4f64e1
Anuraj <aj3095@rit.edu>
2020-09-06 23:47:06 -0700

    Add Words to puzzle (only from left to right support).

---
+++ Word Search/Model/WordsList.swift
@@ -0,0 +1,21 @@
+//
+//  WordsList.swift
+//  Word Search
+//
+//  Created by Anuraj on 9/6/20.
+//  Copyright © 2020 Anuraj. All rights reserved.
+//
+
+import Foundation
+
+struct WordsLists {
+    static let country_list = ["Austria","Bahamas","Bahrain","Belarus","Belgium","Bermuda","Bhutan","Brazil","Chile","China","Colombia","Congo","Denmark","Fiji","Finland","France","Greece","Greenland","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Kenya","Kuwait","Lebanon","Lesotho","Liberia","Libya","Malaysia","Maldives","Mali","Malta","Monaco","Nigeria","Norway","Oman","Poland","Portugal","Qatar","Romania","Russia","Singapore","Slovakia","Sudan","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Turkey","Turkmenistan","Ukraine","Uruguay","Uzbekistan","Venezuela","Vietnam", "US","Yemen","Zambia","Zimbabwe"];
+
+    static let sports_list =
+        [ "Athletics","Archery","Badminton","Baseball",
+          "Basketball","Boxing","Canoeing","Curling","Cricket",
+          "Cycling","Darts","Diving","Fencing","Football",
+          "Gymnastics","Handball","Hockey","Judo","Netball",
+          "Rowing","Sailing","Shooting","Snooker","Squash",
+          "Swimming","Taekwondo","Tennis","Volleyball","Wrestling"]
+}
--- Word Search/View Controller/ArenaViewController.swift
+++ Word Search/View Controller/ArenaViewController.swift
@@ -11,15 +11,88 @@ import UIKit
 class ArenaViewController: UIViewController {
 
     let reuseIdentifier = "letterCell"
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "45", "46", "47", "48", "45", "46", "47", "48", "45", "46", "47", "48", "45", "46", "47", "48"]
+    var wordsList: [String] = []
+    var totalElements = 64
+    var matrix = Array(repeating: Array(repeating: 0, count: 8), count: 8)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getRandomFromList()
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var wordsCollection: UICollectionView!
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func getRandomLetter() -> String {
+        let letters : String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let char = letters.randomElement()!
+        return String(char)
+    }
+    
+    func getRandomFromList() {
+        // make sure word is not repeating and size > row or column
+        for _ in 0...1 {
+            var word = WordsLists.country_list.randomElement()!
+            while (word.count > 8 || wordsList.contains(word)) {
+                word = WordsLists.country_list.randomElement()!
+            }
+            wordsList.append(word)
+        }
+        
+    }
+    
+    func addWordsToCollection() {
+        
+        for word in wordsList {
+            
+            var startRow = Int.random(in: 0..<8)
+            var startColumn = Int.random(in: 0..<8)
+            let wordSize = word.count
+            // if we have space to add in right continue or again get a random value
+            
+            var row = startRow
+            var colm = startColumn
+            var iter = 0
+            
+            while iter != wordSize {
+                
+                if colm >= 8 || row >= 8 || matrix[row][colm] == 1  {
+                    // we have collision or we are out of bounds get a new
+                    iter = 0 // reset iterations
+                    // get new starting row and colmn
+                    startRow = Int.random(in: 0..<8)
+                    startColumn = Int.random(in: 0..<8)
+                    row = startRow
+                    colm = startColumn
+                } else {
+                    iter += 1
+                    //row += 1
+                    colm += 1
+                }
+            }
+            
+            // we got a place to put our word
+            var indx = startRow * 8 + startColumn
+            for char in word {
+            let indxPath = IndexPath(row: indx, section: 0)
+            indx += 1
+            let cell = wordsCollection.cellForItem(at: indxPath) as! WordCollectionViewCell
+                cell.letter.text = String(char).capitalized
+                // testing new entries
+                cell.letter.textColor = UIColor.red
+                //update Matrix
+                matrix[startRow][startColumn] = 1
+                //startRow += 1
+                startColumn += 1
+            }
+            
+        }
 
+    }
     /*
     // MARK: - Navigation
 
@@ -38,7 +111,8 @@ extension ArenaViewController:  UICollectionViewDataSource, UICollectionViewDele
     
     // tell the collection view how many cells to make
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items.count
+        // will be dynamic for different levels
+        return totalElements //self.items.count
     }
     
     // make a cell for each cell index path
@@ -46,18 +120,25 @@ extension ArenaViewController:  UICollectionViewDataSource, UICollectionViewDele
         
         // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! WordCollectionViewCell
-        
-        // Use the outlet in our custom class to get a reference to the UILabel in the cell
-        cell.letter.text = self.items[indexPath.item]
-        //cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+
+        cell.letter.text = getRandomLetter()
+        //cell.backgroundColor = UIColor.cyan
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        if indexPath.item == totalElements - 1 {
+            // done loading all collection view cells
+            addWordsToCollection()
+        }
     }
     
     // MARK: - UICollectionViewDelegate protocol
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // handle tap events
-        print("You selected cell #\(indexPath.item)!")
+        print("You selected cell #\(indexPath.item) and section \(indexPath.section) and  row \(indexPath.row)!")
     }
 }
