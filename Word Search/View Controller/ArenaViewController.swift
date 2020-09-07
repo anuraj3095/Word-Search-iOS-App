73eefcaefa4ddf9b42da16538c44dcfb43ab6694
Anuraj <aj3095@rit.edu>
2020-09-06 21:17:58 -0700

    Add UI

---
+++ Word Search/View Controller/ArenaViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ArenaViewController.swift
+//  Word Search
+//
+//  Created by Anuraj on 9/6/20.
+//  Copyright © 2020 Anuraj. All rights reserved.
+//
+
+import UIKit
+
+class ArenaViewController: UIViewController {
+
+    let reuseIdentifier = "letterCell"
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "45", "46", "47", "48", "45", "46", "47", "48", "45", "46", "47", "48", "45", "46", "47", "48"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ArenaViewController:  UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! WordCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.letter.text = self.items[indexPath.item]
+        //cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+}
---
+++ Word Search/View Controller/WordCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WordCollectionViewCell.swift
+//  Word Search
+//
+//  Created by Anuraj on 9/6/20.
+//  Copyright © 2020 Anuraj. All rights reserved.
+//
+
+import UIKit
+
+class WordCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var letter: UILabel!
+}
